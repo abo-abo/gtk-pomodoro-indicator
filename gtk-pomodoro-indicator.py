593be3724066aa69c992f37d38effcf4f49efc6b
Oleh Krehel <ohwoeowho@gmail.com>
2017-03-27 18:07:36 +0200

    gtk-pomodoro-indicator.py: Require AppIndicator3 v0.1

--- gtk-pomodoro-indicator.py
+++ gtk-pomodoro-indicator.py
@@ -24,6 +24,7 @@ import os
 import signal
 import gi
 gi.require_version ("Gtk", "3.0")
+gi.require_version ("AppIndicator3", "0.1")
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import GObject
